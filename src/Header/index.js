86993931fa957793a40623e7673d49fbbc0e3468
Manuel E. Urena <manuel.ureh@hotmail.com>
2019-06-07 15:28:46 -0400

    Adds Loading, ErrorMessage, Main and Header component

---
+++ src/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="app-header">
+    <div className="header-title">Waldo Pizza</div>
+  </header>
+);
+
+export default Header;
---
+++ src/Main/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+import Loading from '../Loading';
+import ErrorMessage from '../ErrorMessage';
+
+const GET_PIZZA_SIZES = gql`
+  {
+    pizzaSizes {
+      name,
+      maxToppings,
+      basePrice
+    }
+  }
+`;
+
+const Main = () => (
+  <Query query={GET_PIZZA_SIZES}>
+    {({ data, loading, error }) => {
+      if (error) return <ErrorMessage error={error} />;
+      if (loading) return <Loading />;
+
+      const { pizzaSizes } = data;
+
+      return (
+        <div className="main-container">
+          <ul className="pizza-size-list">
+            {pizzaSizes.map(size => (
+              <li key={size.name} className="pizza-size">{size.name} ${size.basePrice}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }}
+  </Query>
+);
+
+export default Main;
